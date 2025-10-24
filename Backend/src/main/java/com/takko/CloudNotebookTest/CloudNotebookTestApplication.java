b781a7b3e8ac2722868a1833bba8dfdc528c24c5
takko <ameato9ei@gmail.com>
2025-10-24 12:37:51 +0900

    use spring initializr to create a maven project with dependencies web, jpa, mysql driver, lombok, devtools and test

---
+++ Backend/src/main/java/com/takko/CloudNotebookTest/CloudNotebookTestApplication.java
@@ -0,0 +1,13 @@
+package com.takko.CloudNotebookTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudNotebookTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudNotebookTestApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/takko/CloudNotebookTest/CloudNotebookTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takko.CloudNotebookTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNotebookTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
